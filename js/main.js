535ba519a88665b2f3612f0e7f6f833f5d843419
Ksenia Tuikova <ksenia.t0809@gmail.com>
2023-03-14 00:25:37 +0200

    Nursing school steps

--- js/main.js
+++ js/main.js
@@ -5,4 +5,56 @@ $(document).ready(function() {
         $('body').toggleClass('overflow');
         e.preventDefault();
     })
-})
+});
+
+// Custom Select
+$('.custom-select').each(function(){
+    var $this = $(this), numberOfOptions = $(this).children('option').length;
+  
+    $this.addClass('select-hidden'); 
+    $this.wrap('<div class="select"></div>');
+    $this.after('<div class="select-styled"></div>');
+    $this.next('div.select-styled').addClass('not-selected');
+
+    var $styledSelect = $this.next('div.select-styled');
+    $styledSelect.text($this.children('option').eq(0).text());
+  
+    var $list = $('<ul />', {
+        'class': 'select-options'
+    }).insertAfter($styledSelect);
+  
+    for (var i = 0; i < numberOfOptions; i++) {
+        $('<li />', {
+            text: $this.children('option').eq(i).text(),
+            rel: $this.children('option').eq(i).val()
+        }).appendTo($list);
+        if ($this.children('option').eq(i).is(':selected')){
+         $('li[rel="' + $this.children('option').eq(i).val() + '"]').addClass('is-selected')
+        }
+    }
+  
+    var $listItems = $list.children('li');
+  
+    $styledSelect.click(function(e) {
+        e.stopPropagation();
+        $('div.select-styled.active').not(this).each(function(){
+            $(this).removeClass('active').next('ul.select-options').hide();
+        });
+        $(this).toggleClass('active').next('ul.select-options').toggle();
+    });
+  
+    $listItems.click(function(e) {
+        e.stopPropagation();
+        $styledSelect.text($(this).text()).removeClass('active');
+        $styledSelect.text($(this).text()).removeClass('not-selected');
+        $this.val($(this).attr('rel'));
+        $list.hide();
+        //console.log($this.val());
+    });
+  
+    $(document).click(function() {
+        $styledSelect.removeClass('active');
+        $list.hide();
+    });
+
+});
